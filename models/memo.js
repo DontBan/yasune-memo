cf21ec5662b0bc34d5b4fc5248da0ca0d17dadaf
Kobayashi Tomohiro <17561732+DontBan@users.noreply.github.com>
2019-07-16 08:45:08 +0000

    データモデリングしました

---
+++ models/memo.js
@@ -0,0 +1,45 @@
+'use strict';
+const loader = require('./sequelize-loader');
+const Sequelize = loader.Sequelize;
+
+const Memo = loader.database.define('memos', {
+  memoId: {
+    type: Sequelize.UUID,
+    primaryKey: true,
+    allowNull: false
+  },
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shopName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  genre: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  isPricedown: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
+}, {
+    freezeTableName: true,
+    timestamps: false,
+    indexes: [
+      {
+        fields: ['createdBy']
+      }
+    ]
+  });
+
+module.exports = Memo;
