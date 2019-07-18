6d4c056b9f46c01a9afc4a8b4a417321c18ef846
Kobayashi Tomohiro <17561732+DontBan@users.noreply.github.com>
2019-07-18 05:04:07 +0000

    安値メモの入力フォームデータをDBに保存を実装

--- routes/memos.js
+++ routes/memos.js
@@ -2,14 +2,29 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
+const uuid = require('uuid');
+const Memo = require('../models/memo');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', { user: req.user });
 });
 
 router.post('/', authenticationEnsurer, (req, res, next) => {
-  console.log(req.body); // TODO メモを保存する実装をする
-  res.redirect('/');
+  // console.log(req.body);
+  // res.redirect('/');
+
+  const memoId = uuid.v4();
+  const updatedAt = new Date();
+  Memo.create({
+    memoId: memoId,
+    productName: req.body.productName.slice(0, 255),
+    price: req.body.price,
+    shopName: req.body.shopName.slice(0, 255),
+    remarks: req.body.remarks.slice(0, 255),
+    updatedAt: updatedAt
+  }).then((memo) => {
+    res.redirect('/memos/' + memo.memoId);
+  });
 });
 
 module.exports = router;
